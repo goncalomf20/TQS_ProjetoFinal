a530ebbea1913d9ba76ec84d201ad42690adbad6
Sara Almeida <sarafalmeida@ua.pt>
2024-05-08 00:44:13 +0100

    spring setup

---
+++ DETICafe/src/test/java/tqs_project/DETICafe/DetiCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package tqs_project.DETICafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetiCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
