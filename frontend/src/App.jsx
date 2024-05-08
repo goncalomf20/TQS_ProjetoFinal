99eba923b5c794244cd5923d6164a559242eb660
JoaoCLS <joao.carlos.santos@ua.pt>
2024-05-08 17:37:06 +0100

    Kitchen pototipo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Correct import
-import Kiosk from './components/Kiosk';
 import { Checkout } from './components/Checkout';
-import { SidebarDC } from './components/Sidebar';
-import { CartButton } from './components/CartButton';
 import { PaymentChoice } from './components/PaymentChoice';
 import { WaitingScreen } from './components/WaitingScreen';
-import { Kitchen } from './components/Kitchen';
-import { Service } from './components/Service';
+import  Kitchen  from './components/Kitchen';
+{/*import  Service  from './components/Service'; */}
 import KioskSide from './KioskSide';
 
 
@@ -22,7 +19,7 @@ function App() {
             <Route path="/checkout" element={<Checkout cart={cart} />} />
             <Route path="/payment-choice" element={<PaymentChoice />} />
             <Route path="/kitchen" element={<Kitchen />} />
-            <Route path="/service" element={<Service />} />
+            {/*<Route path="/service" element={<Service />} />*/}
           </Routes>
     </Router>
   );
