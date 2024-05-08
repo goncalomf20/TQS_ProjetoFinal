550acbc510a92974f67e1feb6475d36f8068876e
goncalomf <goncalomf@ua.pt>
2024-05-08 15:44:01 +0100

    fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,40 +6,20 @@ import { SidebarDC } from './components/Sidebar';
 import { CartButton } from './components/CartButton';
 import { PaymentChoice } from './components/PaymentChoice';
 import { WaitingScreen } from './components/WaitingScreen';
+import KioskSide from './KioskSide';
 
 
 function App() {
   const [cart, setCart] = React.useState([]);
 
-  const handleAddToCart = (food_id, price) => { // Accept price as a parameter
-    const updatedCart = [...cart];
-
-    const existingItem = updatedCart.find((item) => item.food_id === food_id);
-    if (existingItem) {
-      existingItem.quantity += 1;
-    } else {
-      updatedCart.push({ food_id, quantity: 1, price }); // Use the passed price
-    }
-
-    setCart(updatedCart);
-  };
-
-  const cartQuantity = cart.reduce((total, item) => total + item.quantity, 0);
-
   return (
     <Router>
-      <div className="flex min-h-screen relative">
-        <SidebarDC />
-        <div className="flex-1 p-4">
-          <Routes> {/* Use Routes instead of Switch */}
-            <Route path="/" element={<Kiosk onAddToCart={handleAddToCart} cart={cart} />} /> {/* Use element prop */}
+          <Routes> 
+            <Route path="/" element={<KioskSide  />} /> {/* Use element prop */}
+            <Route path="/waiting-screen" element={<WaitingScreen />} />
             <Route path="/checkout" element={<Checkout cart={cart} />} />
             <Route path="/payment-choice" element={<PaymentChoice />} />
-            <Route path="/waiting-screen" element={<WaitingScreen />} />
           </Routes>
-        </div>
-        <CartButton cartQuantity={cartQuantity} />
-      </div>
     </Router>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <WaitingApp /> */}
     <App />
   </React.StrictMode>,
 )
