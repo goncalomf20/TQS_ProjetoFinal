232daa5ea6cdcdb1fa26af5b56ebd59ca0064af0
goncalomf <goncalomf@ua.pt>
2024-05-22 00:14:29 +0100

    close branch

--- frontend/src/components/Kiosk.jsx
+++ frontend/src/components/Kiosk.jsx
@@ -3,72 +3,80 @@ import FoodCardsList from './FoodList';
 import { useSearchParams } from 'react-router-dom';
 
 function Kiosk({ onAddToCart, cart }) {  
+  const [allFoods, setAllFoods] = useState([]); // State for all foods
+  
+  // setAllFoods([
+  //   {
+  //     id: 1,
+  //     name: 'Pizza',
+  //     image: '/src/assets/pizza.jpeg',
+  //     description: 'A delicious cheese pizza with a crispy crust.',
+  //     price: 11.99,
+  //     category: 2,
+  //     ingredients: ['cheese', 'tomato sauce', 'flour'],
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Sushi',
+  //     image: '/src/assets/sushi.jpg',
+  //     description: 'Fresh sushi with salmon and avocado.',
+  //     price: 12.99,
+  //     category: 2,
+  //     ingredients: ['salmon', 'rice', 'avocado'],
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Burger',
+  //     image: '/src/assets/burguer.jpg',
+  //     description: 'Juicy beef patty with fresh vegetables and cheese.',
+  //     price: 9.99,
+  //     category: 2,
+  //     ingredients: ['beef', 'lettuce', 'tomato'],
+  //   },
+  //   {
+  //     id: 4,
+  //     name: 'Pasta',
+  //     image: '/src/assets/pasta.jpeg',
+  //     description: 'Spaghetti with tomato sauce and meatballs.',
+  //     price: 8.99,
+  //     category: 2,
+  //     ingredients: ['spaghetti', 'tomato sauce', 'meatballs'],
+  //   },
+  //   {
+  //     id: 5,
+  //     name: 'Ham and Cheese Croissant',
+  //     image: '/src/assets/ham_and_cheese.jpeg',
+  //     description: 'Freshly baked croissant with ham and cheese.',
+  //     price: 3.99,
+  //     category: 5,
+  //     ingredients: ['croissant', 'ham', 'cheese'],
+  //   },
+  //   {
+  //     id: 6,
+  //     name: 'Large Coffee',
+  //     image: '/src/assets/large_coffee.jpg',
+  //     description: 'A large cup of freshly brewed coffee.',
+  //     price: 1.99,
+  //     category: 6,
+  //     ingredients: ['coffee', 'water', 'sugar'],
+  //   },
+  //   {
+  //     id: 7,
+  //     name: 'Tuna Sandwich',
+  //     image: '/src/assets/tuna_sandwich.jpg',
+  //     description: 'A classic tuna sandwich with lettuce and mayo.',
+  //     price: 1.99,
+  //     category: 3,
+  //     ingredients: ['bread', 'tuna', 'lettuce' , 'mayo' , 'tomato'],
+  //   },
+  // ]);
 
-  const allFoods = [
-    {
-      id: 1,
-      name: 'Pizza',
-      image: '/src/assets/pizza.jpeg',
-      description: 'A delicious cheese pizza with a crispy crust.',
-      price: 11.99,
-      category: 2,
-      ingredients: ['cheese', 'tomato sauce', 'flour'],
-    },
-    {
-      id: 2,
-      name: 'Sushi',
-      image: '/src/assets/sushi.jpg',
-      description: 'Fresh sushi with salmon and avocado.',
-      price: 12.99,
-      category: 2,
-      ingredients: ['salmon', 'rice', 'avocado'],
-    },
-    {
-      id: 3,
-      name: 'Burger',
-      image: '/src/assets/burguer.jpg',
-      description: 'Juicy beef patty with fresh vegetables and cheese.',
-      price: 9.99,
-      category: 2,
-      ingredients: ['beef', 'lettuce', 'tomato'],
-    },
-    {
-      id: 4,
-      name: 'Pasta',
-      image: '/src/assets/pasta.jpeg',
-      description: 'Spaghetti with tomato sauce and meatballs.',
-      price: 8.99,
-      category: 2,
-      ingredients: ['spaghetti', 'tomato sauce', 'meatballs'],
-    },
-    {
-      id: 5,
-      name: 'Ham and Cheese Croissant',
-      image: '/src/assets/ham_and_cheese.jpeg',
-      description: 'Freshly baked croissant with ham and cheese.',
-      price: 3.99,
-      category: 5,
-      ingredients: ['croissant', 'ham', 'cheese'],
-    },
-    {
-      id: 6,
-      name: 'Large Coffee',
-      image: '/src/assets/large_coffee.jpg',
-      description: 'A large cup of freshly brewed coffee.',
-      price: 1.99,
-      category: 6,
-      ingredients: ['coffee', 'water', 'sugar'],
-    },
-    {
-      id: 7,
-      name: 'Tuna Sandwich',
-      image: '/src/assets/tuna_sandwich.jpg',
-      description: 'A classic tuna sandwich with lettuce and mayo.',
-      price: 1.99,
-      category: 3,
-      ingredients: ['bread', 'tuna', 'lettuce' , 'mayo' , 'tomato'],
-    },
-  ];
+  useEffect(() => {
+    fetch("http://localhost:8080/api/products/getAllProducts")
+      .then((response) => response.json())
+      .then((data) => setAllFoods(data)); // Set categories from API response
+  }
+  , []); 
 
   const [foods, setFoods] = useState(allFoods);
 
@@ -78,7 +86,7 @@ function Kiosk({ onAddToCart, cart }) {
     const category = searchParams.get('category'); // Retrieve the 'category' query parameter
     if (category) {
       const filteredFoods = allFoods.filter(
-        (food) => food.category === parseInt(category)
+        (food) => food.category.categoryId === parseInt(category)
       );
       setFoods(filteredFoods); // Update state with filtered foods
     } else {
