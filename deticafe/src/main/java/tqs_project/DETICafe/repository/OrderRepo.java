83e05344b48573384bff356c556a6f88261a43e9
Sara Almeida <sarafalmeida@ua.pt>
2024-05-22 00:54:36 +0100

    testing and pom alteration

--- deticafe/src/main/java/tqs_project/DETICafe/repository/OrderRepo.java
+++ deticafe/src/main/java/tqs_project/DETICafe/repository/OrderRepo.java
@@ -1,6 +1,9 @@
 package tqs_project.DETICafe.repository;
 
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import tqs_project.DETICafe.model.Order;
@@ -9,5 +12,6 @@ import tqs_project.DETICafe.model.Order;
 public interface OrderRepo extends JpaRepository<Order, Long>{
 
     public Order findByOrderId(long orderId);
+    public List<Order> findAll();
     
 }
