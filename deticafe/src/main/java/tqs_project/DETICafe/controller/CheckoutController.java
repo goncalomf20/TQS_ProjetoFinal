51c0578db5478a486e2fe86662ec42b32210e059
JoaoCLS <joao.carlos.santos@ua.pt>
2024-05-23 19:33:16 +0100

    spring socket config ( missing front-end config)

--- deticafe/src/main/java/tqs_project/DETICafe/controller/CheckoutController.java
+++ deticafe/src/main/java/tqs_project/DETICafe/controller/CheckoutController.java
@@ -1,20 +1,12 @@
 package tqs_project.DETICafe.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map.Entry;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.web.bind.annotation.*;
 
 import tqs_project.DETICafe.DTO.OrderDetailsDTO;
 import tqs_project.DETICafe.model.Order;
@@ -24,9 +16,12 @@ import tqs_project.DETICafe.repository.OrderRepo;
 import tqs_project.DETICafe.service.OrderService;
 import tqs_project.DETICafe.service.ProductService;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+
 @RestController
 @RequestMapping("/api/order")
-@CrossOrigin(origins = "*")
 public class CheckoutController {
 
     private final OrderService orderService;
@@ -35,7 +30,6 @@ public class CheckoutController {
 
     @Autowired
     private SimpMessagingTemplate template;
-
 
     @Autowired
     public CheckoutController(OrderService orderService, ProductService productService, OrderRepo orderRepo) {
@@ -48,42 +42,28 @@ public class CheckoutController {
     public ResponseEntity<String> createOrder(@RequestBody List<OrderDetailsDTO> orderDetailsList) {
         List<OrderDetails> orderDetails = new ArrayList<>();
         for (OrderDetailsDTO orderDetailsDTO : orderDetailsList) {
-            List<String> costumizations = new ArrayList<>();
-            System.out.println(orderDetailsDTO.getName() + " name");
-            System.out.println(orderDetailsDTO.getQuantity() + " quantity");
-            System.out.println(orderDetailsDTO.getFoodId() + " foodId");
-            System.out.println(orderDetailsDTO.getOrderDetails() + " orderDetails");
-
+            List<String> customizations = new ArrayList<>();
             Product product = productService.getProductById(orderDetailsDTO.getFoodId());
-            System.out.println(product.getName() + " product name");
 
             for (Entry<String, Boolean> entry : orderDetailsDTO.getOrderDetails().entrySet()) {
-                System.out.println(entry.getKey() + " key");
-                System.out.println(entry.getValue() + " value");
                 if (entry.getValue()) {
-                    costumizations.add(entry.getKey());
+                    customizations.add(entry.getKey());
                 }
             }
 
-            OrderDetails orderDetail = new OrderDetails(1l ,costumizations, product);
+            OrderDetails orderDetail = new OrderDetails(customizations, product);
+            System.out.println("OrderDetail created: " + orderDetail.toString());
             orderDetails.add(orderDetail);
         }
 
         Order order = new Order(orderDetails);
         orderRepo.save(order);
 
-        System.out.println(order.getOrderDetails());
-        System.out.println(order.getOrderId());
+        System.out.println("Order created: " + order.getOrderId() + "with details: " + orderDetailsList.toString());
 
-        if (order!= null && order.getOrderId()!= null) {
-            template.convertAndSend("/topic/orders", order.getOrderId());
-        } else {
-            // Log ou trate o caso em que a ordem ou o ID da ordem é nulo
-            System.err.println("Erro: A ordem ou o ID da ordem é nulo.");
-        }
-        
+        // Enviar ordem via WebSocket
+        template.convertAndSend("/topic/orders", order);
 
-        
         return new ResponseEntity<>("ok", HttpStatus.OK);
     }
 
@@ -92,5 +72,4 @@ public class CheckoutController {
         Order order = orderService.getOrder(id);
         return new ResponseEntity<Order>(order, HttpStatus.OK);
     }
-    
 }
